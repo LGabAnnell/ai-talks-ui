d9216037bf0c713ecfaf812b56ca56b2888eead7
Gabriel Annell <gabriel@annell.ch>
2024-12-12 06:30:04 +0100

    pass assistant and user model in initial request

--- src/components/Accept.tsx
+++ src/components/Accept.tsx
@@ -1,27 +1,29 @@
-import { AppDispatch, RootState } from "../store";
-import { useDispatch, useSelector } from "react-redux";
-import { doInitialRequest } from "../http";
-import { updateConversationId, updateMessages } from "../store/reducers/input-action-reducers.ts";
+import {AppDispatch, RootState} from "../store";
+import {useDispatch, useSelector} from "react-redux";
+import {doInitialRequest} from "../http";
+import {updateConversationId, updateMessages} from "../store/reducers/input-action-reducers.ts";
 
 export const Accept = () => {
-  const {
-    modelInfos,
-    initialMessage
-  } = useSelector((state: RootState) => state.inputStateReducer)
-  const dispatch: AppDispatch = useDispatch();
-  return (
-    <div>
-      <button onClick={() =>
-        doInitialRequest(
-          modelInfos.find(modelInfo => modelInfo.type === 'user')?.systemInstructions ?? "",
-          initialMessage
-        ).then(result => {
-          dispatch(updateMessages(result));
-          dispatch(updateConversationId(result.conversationId));
-        })
-      }>
-        Accept
-      </button>
-    </div>
-  )
+    const {
+        modelInfos,
+        initialMessage
+    } = useSelector((state: RootState) => state.inputStateReducer)
+    const dispatch: AppDispatch = useDispatch();
+    return (
+        <div>
+            <button onClick={() =>
+                doInitialRequest(
+                    modelInfos.find(modelInfo => modelInfo.type === 'user')?.model ?? "",
+                    modelInfos.find(modelInfo => modelInfo.type === 'assistant')?.model ?? "",
+                    modelInfos.find(modelInfo => modelInfo.type === 'user')?.systemInstructions ?? "",
+                    initialMessage
+                ).then(result => {
+                    dispatch(updateMessages(result));
+                    dispatch(updateConversationId(result.conversationId));
+                })
+            }>
+                Accept
+            </button>
+        </div>
+    )
 }
--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,22 +1,29 @@
-import { defaultRequest } from "../model/initial-request.ts";
-import { InitialResponse } from "../model/initial-response.ts";
-import { ContinuationResponse } from "../model/continuation-response.ts"; // Ensure to import your RootState type
+import {defaultRequest} from "../model/initial-request.ts";
+import {InitialResponse} from "../model/initial-response.ts";
+import {ContinuationResponse} from "../model/continuation-response.ts"; // Ensure to import your RootState type
 
-export const doInitialRequest = async (systemInstructions: string, initialMessage: string): Promise<InitialResponse>  => {
-  return await fetch('http://localhost:8080/initiate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      ...defaultRequest,
-      systemInstructions: systemInstructions,
-      message: initialMessage
-    })
-  }).then(response => response.json());
+export const doInitialRequest = async (
+    assistantModel: string,
+    userModel: string,
+    systemInstructions: string,
+    initialMessage: string
+): Promise<InitialResponse> => {
+    return await fetch('http://localhost:8080/initiate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ...defaultRequest,
+            assistantModel: assistantModel,
+            userModel: userModel,
+            systemInstructions: systemInstructions,
+            message: initialMessage
+        })
+    }).then(response => response.json());
 };
 
 export const nextRequest = async (conversationId: number): Promise<ContinuationResponse> => {
-  return await fetch(`http://localhost:8080/next?conversationId=${conversationId}`)
-    .then(response => response.json());
+    return await fetch(`http://localhost:8080/next?conversationId=${conversationId}`)
+        .then(response => response.json());
 }
